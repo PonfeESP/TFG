71103158cac322483388876d3653d88bda9ca379
PonfeESP <128382547+PonfeESP@users.noreply.github.com>
2024-05-20 17:11:13 +0200

    s

--- Server/conexiondb.js
+++ Server/conexiondb.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 
 let conexiondb;
 
-const enlace = (resultado) => {
+/*const enlace = (resultado) => {
     mongoose.connect('mongodb://127.0.0.1:27017/ITJobFinder')
         .then(() => {
             conexiondb = mongoose.connection;
@@ -13,6 +13,21 @@ const enlace = (resultado) => {
             console.log('Fallo al conectar con la base de datos', err);
             return resultado(err);
         });
+};*/
+
+const enlace = (resultado) => {
+    mongoose.connect('mongodb+srv://cassergiocanedo:UDhib3bpJQrf1Jq4@itjobfinder.dqcdtv3.mongodb.net/?retryWrites=true&w=majority&appName=ITJobFinder', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        conexiondb = mongoose.connection;
+        return resultado();
+    })
+    .catch(err => {
+        console.log('Fallo al conectar con la base de datos', err);
+        return resultado(err);
+    });
 };
 
 const llamardb = () => conexiondb;
